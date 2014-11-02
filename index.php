ff691c1a3d6b66c648974f6bea9eb2538ce9f5b9
Samir Patel <sameg14@gmail.com>
2014-11-01 19:08:45 -0500

    Added config and ignore

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+define('CLIENT_SECRET', getenv('CLIENT_SECRET'));
+define('CLIENT_ID', getenv('CLIENT_ID'));
+define('EMAIL_ADDRESS', getenv('EMAIL_ADDRESS'));
+define('API_KEY', getenv('API_KEY'));
+define('REDIRECT_URI', getenv('REDIRECT_URI'));
